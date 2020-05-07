4b947b157831e05b6fe769bb6087ddf70afad7f5
Tamdaiphuc <64886714+Tamdaiphuc@users.noreply.github.com>
2020-05-07 14:42:53 +0700

    Add files via upload

---
+++ codelienhe.js
@@ -0,0 +1,28 @@
+ <script type='text/javascript'>
+//<![CDATA[
+  <div class='giuseart-nav'>
+        <ul>
+            <li><a href='https://www.google.com/maps/place/GiuseArt+-+Thi%E1%BA%BFt+k%E1%BA%BF+%C4%91%E1%BB%93+h%E1%BB%8Da+v%C3%A0+website+chuy%C3%AAn+nghi%E1%BB%87p/@20.3534004,105.894187,17z/data=!3m1!4b1!4m5!3m4!1s0x3135acc6066cb537:0x4fc1c256d7cdb4d!8m2!3d20.3533954!4d105.8963757' rel='nofollow' target='_blank'><i class='ticon-heart'/>Tìm du?ng</a></li>
+            <li><a href='https://zalo.me/0972939830' rel='nofollow' target='_blank'><i class='ticon-zalo-circle2'/>Chat Zalo</a></li>
+                        <li class='phone-mobile'>
+                            <a class='button' href='tel:0972939830' rel='nofollow'>
+                                <span class='phone_animation animation-shadow'>
+                                    <i aria-hidden='true' class='icon-phone-w'/>
+                                </span>
+                                <span class='btn_phone_txt'>G?i di?n</span>
+                            </a>
+                        </li>
+                        <li><a href='https://www.messenger.com/t/joseph.thien.75' rel='nofollow' target='_blank'><i class='ticon-messenger'/>Messenger</a></li>
+            <li><a class='chat_animation' href='sms:0972939830'>
+            <i aria-hidden='true' class='ticon-chat-sms' title='Nh?n tin sms'/>
+                Nh?n tin SMS</a>
+            </li>
+            <li class='to-top-pc'>
+                <a href='#' rel='nofollow'>
+                    <i aria-hidden='true' class='ticon-angle-up' title='Quay lên trên'/>
+                </a>
+            </li>
+        </ul>
+    </div>
+  //]]>
+  </script>
