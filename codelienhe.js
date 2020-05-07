a2f8b7637d43b275d54c64aaa4f4983e9d964c16
Tamdaiphuc <64886714+Tamdaiphuc@users.noreply.github.com>
2020-05-07 14:58:28 +0700

    Update codelienhe.js

--- codelienhe.js
+++ codelienhe.js
@@ -1,8 +1,6 @@
- <script type='text/javascript'>
-//<![CDATA[
-  <div class='giuseart-nav'>
+<div class='giuseart-nav'>
         <ul>
-            <li><a href='https://www.google.com/maps/place/GiuseArt+-+Thi%E1%BA%BFt+k%E1%BA%BF+%C4%91%E1%BB%93+h%E1%BB%8Da+v%C3%A0+website+chuy%C3%AAn+nghi%E1%BB%87p/@20.3534004,105.894187,17z/data=!3m1!4b1!4m5!3m4!1s0x3135acc6066cb537:0x4fc1c256d7cdb4d!8m2!3d20.3533954!4d105.8963757' rel='nofollow' target='_blank'><i class='ticon-heart'/>Tìm du?ng</a></li>
+            <li><a href='https://www.google.com/maps/place/GiuseArt+-+Thi%E1%BA%BFt+k%E1%BA%BF+%C4%91%E1%BB%93+h%E1%BB%8Da+v%C3%A0+website+chuy%C3%AAn+nghi%E1%BB%87p/@20.3534004,105.894187,17z/data=!3m1!4b1!4m5!3m4!1s0x3135acc6066cb537:0x4fc1c256d7cdb4d!8m2!3d20.3533954!4d105.8963757' rel='nofollow' target='_blank'><i class='ticon-heart'/>TÃ¬m du?ng</a></li>
             <li><a href='https://zalo.me/0972939830' rel='nofollow' target='_blank'><i class='ticon-zalo-circle2'/>Chat Zalo</a></li>
                         <li class='phone-mobile'>
                             <a class='button' href='tel:0972939830' rel='nofollow'>
@@ -19,10 +17,8 @@
             </li>
             <li class='to-top-pc'>
                 <a href='#' rel='nofollow'>
-                    <i aria-hidden='true' class='ticon-angle-up' title='Quay lên trên'/>
+                    <i aria-hidden='true' class='ticon-angle-up' title='Quay lÃªn trÃªn'/>
                 </a>
             </li>
         </ul>
     </div>
-  //]]>
-  </script>
